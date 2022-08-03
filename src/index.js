33403700a95178b58ee8dd648397634eb66e5f8a
Fraser Redford <57697667+fredford@users.noreply.github.com>
2022-08-03 13:08:01 -0600

    remove old SCSS files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+// Library imports
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-
+// CSS imports
+import "./index.css";
+// Redux store
 import store from "./store";
+// Context Providers
 import { SettingsProvider } from "./contexts/settingsContext";
 import { WorkoutProvider } from "./contexts/workoutContext";
+// Local component imports
+import App from "./App";
 
 function start() {
   ReactDOM.render(
--- src/services/services.js
+++ src/services/services.js
@@ -2,7 +2,7 @@ import UserService from "./UserService";
 import WorkoutService from "./WorkoutService";
 import WorkoutsService from "./WorkoutsService";
 import SetsService from "./SetsService";
-import StatsService from "./StatsService"
+import StatsService from "./StatsService";
 import ExercisesService from "./ExercisesService";
 
 const services = {
@@ -11,7 +11,7 @@ const services = {
   workouts: WorkoutsService,
   sets: SetsService,
   stats: StatsService,
-  exercises: ExercisesService
-}
+  exercises: ExercisesService,
+};
 
 export default services;
